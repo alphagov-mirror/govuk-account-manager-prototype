8e7de614e6860055b860a361a24c9a30886628d2
Bruce Bolt <bruce.bolt@digital.cabinet-office.gov.uk>
2020-08-18 13:18:52 +0100

    Show the transition checker data in the account manager

--- app/helpers/profile_helper.rb
+++ app/helpers/profile_helper.rb
@@ -5,6 +5,7 @@ module ProfileHelper
     [
       attribute(:email, user_info[:email_verified] ? user_info[:email] : "#{user_info[:email]} (unverified)"),
       attribute(:test, Rails.env.production? ? nil : user_info[:test]),
+      attribute(:transition_checker_state, user_info[:transition_checker_state]),
     ].reject { |detail| detail.fetch(:value).blank? }
   end
 
