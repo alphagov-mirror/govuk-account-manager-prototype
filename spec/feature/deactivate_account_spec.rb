2af74d4b89588e36b31ef4a0b1bbb434eb4bf33b
Huw Diprose <huw.diprose@digital.cabinet-office.gov.uk>
2020-12-18 10:08:58 +0000

    TOFIXUP: experiement with other ways of reloading routes in Rspec tests
    Note this breaks all the tests and is not final

--- spec/feature/deactivate_account_spec.rb
+++ spec/feature/deactivate_account_spec.rb
@@ -1,65 +1,51 @@
 RSpec.feature "Deactivate account by flag" do
-
-  context "For an unauthenticated session" do
-    scenario "A user sees a sevice deactivated message when trying to log in" do
-      given_accounts_are_disabled_by_feature_flag do
-        when_i_visit_the_login_page
-        then_i_see_account_disabled_page_content
-      end
-    end
-
-    scenario "A user sees a sevice deactivated message when trying to sign up" do
-      given_accounts_are_disabled_by_feature_flag do
-        when_i_visit_the_signup_page
-        then_i_see_account_disabled_page_content
-      end
-    end
-
-    scenario "A user sees a sevice deactivated message when trying to leave feedback" do
-      given_accounts_are_disabled_by_feature_flag do
-        and_i_visit_the_feedback_page
-        then_i_see_account_disabled_page_content
-      end
-    end
-  end
-
-  context "For an authenticated session" do
-    scenario "A user is logged out and sees a service deactivated message when visiting the account root page" do
-      given_accounts_are_disabled_by_feature_flag do
-        given_i_am_logged_in
-        when_i_visit_the_account_root_page
-        then_i_see_account_disabled_page_content
-      end
-    end
-
-    scenario "A user is logged out and sees a service deactivated message when visiting the manage account page" do
-      given_accounts_are_disabled_by_feature_flag do
-        given_i_am_logged_in
-        when_i_visit_the_manage_page
-        then_i_see_account_disabled_page_content
-      end
-    end
-
-    scenario "A user is logged out and sees a service deactivated message when visiting the account security page" do
-      given_accounts_are_disabled_by_feature_flag do
-        given_i_am_logged_in
-        when_i_visit_the_security_page
-        then_i_see_account_disabled_page_content
-      end
-    end
-
-    scenario "A user is logged out and sees a service deactivated message when visiting the give feedback page" do
-      given_accounts_are_disabled_by_feature_flag do
-        given_i_am_logged_in
-        when_i_visit_the_feedback_page
-        then_i_see_account_disabled_page_content
-      end
-    end
-  end
-
-  def given_accounts_are_disabled_by_feature_flag(&block)
+  context "Given the disable account feature flag is set to disabled" do
     ClimateControl.modify FEATURE_FLAG_ACCOUNTS: "disabled" do
-      block.call
+      before { Rails.application.reload_routes! }
+      after { Rails.application.reload_routes! }
+
+      context "For an unauthenticated session" do
+        scenario "A user sees a sevice deactivated message when trying to log in" do
+          when_i_visit_the_login_page
+          then_i_see_account_disabled_page_content
+        end
+
+        scenario "A user sees a sevice deactivated message when trying to sign up" do
+          when_i_visit_the_signup_page
+          then_i_see_account_disabled_page_content
+        end
+
+        scenario "A user sees a sevice deactivated message when trying to leave feedback" do
+          when_i_visit_the_feedback_page
+          then_i_see_account_disabled_page_content
+        end
+      end
+
+      context "For an authenticated session" do
+        scenario "A user is logged out and sees a service deactivated message when visiting the account root page" do
+          given_i_am_logged_in
+          when_i_visit_the_account_root_page
+          then_i_see_account_disabled_page_content
+        end
+
+        scenario "A user is logged out and sees a service deactivated message when visiting the manage account page" do
+          given_i_am_logged_in
+          when_i_visit_the_manage_page
+          then_i_see_account_disabled_page_content
+        end
+
+        scenario "A user is logged out and sees a service deactivated message when visiting the account security page" do
+          given_i_am_logged_in
+          when_i_visit_the_security_page
+          then_i_see_account_disabled_page_content
+        end
+
+        scenario "A user is logged out and sees a service deactivated message when visiting the give feedback page" do
+          given_i_am_logged_in
+          when_i_visit_the_feedback_page
+          then_i_see_account_disabled_page_content
+        end
+      end
     end
   end
 
